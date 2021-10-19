2d401c351dff2330328c72b015c7380b16dd614f
Cibsantos <67697559+Cibsantos@users.noreply.github.com>
2021-10-19 13:16:47 -0300

    feat/using Header and Footer in app

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Footer  from './Components/Footer/Footer';
+import Header  from './Components/Header/Header';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <App />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
