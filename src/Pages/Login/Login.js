f7a35392dc909f462827f7ade40c6aea7c8268e9
Cibsantos <67697559+Cibsantos@users.noreply.github.com>
2021-10-19 12:01:32 -0300

    feat/mounted the login page html

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -5,7 +5,29 @@ import logo from '../../Assets/images/logo-ewally.png';
 
 
 export default class Login extends Component {
-  render(){
-    return <p>Login</p>
+  render() {
+    return (
+      <div className="login">
+        <div className="container">
+          <div className="logo" >
+            <img src={logo} alt="eWally" width= "90px" />
+            <h3>A conta digital Feita pra você</h3>
+          </div>         
+          <form className="form-group col-md-4 mx-auto" >
+            <input type="text" name="username" id="username" placeholder="Usuário:" className="form-control"  />
+            <div className="errorBox">
+             
+            </div>
+            <input type="password" name="password" id="password" placeholder="Senha"  className="form-control" />
+            <div className="errorBox">
+           
+            </div>
+            <div>
+              <button type="submit" className="btn btn-primary col-md-12">Entrar</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
   }
-}
+};
