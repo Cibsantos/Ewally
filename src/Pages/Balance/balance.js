2aaaaea0c55630521945d0c8a951a76452b2eddf
Cibsantos <67697559+Cibsantos@users.noreply.github.com>
2021-10-19 11:47:10 -0300

    feat/created pages folder to organize login and account statement pages

---
+++ src/Pages/Balance/balance.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './balance.css';
+
+import logo from '../../Assets/images/logo-ewally.png'; 
+
+export default class Balance extends Component {
+   render(){
+     return <p>Balance</p>
+   }
+}
---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './login.css';
+import logo from '../../Assets/images/logo-ewally.png';
+
+
+export default class Login extends Component {
+  render(){
+    return <p>Login</p>
+  }
+}
