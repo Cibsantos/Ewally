4973341c10085a2a9ff989f265cc974823cd2f17
Cibsantos <67697559+Cibsantos@users.noreply.github.com>
2021-10-20 13:26:41 -0300

    fix/removed footer from index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Footer  from './Components/Footer/Footer';
 import Header  from './Components/Header/Header';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
     <App />
-    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
